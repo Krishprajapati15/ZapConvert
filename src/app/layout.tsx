9533301abd5acacb775410c9f23605dba654b715
Krishprajapati15 <prajapatikrish132005@gmail.com>
2025-04-26 00:50:02 +0530

    change the location

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ export const metadata: Metadata = {
   creator: "Krish Prajapati",
   keywords:
     "file converter, image converter, video converter, audio converter, free file conversion, unlimited file converter",
+  icons: {
+    icon: "./favicon.ico",
+  },
 };
 
 export default function RootLayout({
